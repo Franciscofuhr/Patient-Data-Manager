7935ce87997958e86b1019fd2550507b1207d846
Francisco Jose Fuhr <francisco.fuhr@valtech.com>
2025-10-20 11:25:22 -0300

    Add: Error Component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
+import ErrorState from "./components/ErrorState/ErrorState";
 import Header from "./components/Header/Header";
 import Loader from "./components/Loader/Loader";
 import PatientList from "./components/PatientList/PatientList";
 import { usePatients } from "./context/PatientsContext";
 
-
 function App() {
-  const { loading } = usePatients();
+  const { loading, error } = usePatients();
 
   return (
     <>
       <div className="contentContainer">
         <Header />
-        {loading ? <Loader /> : <PatientList />}
+        {loading ? <Loader /> : error ? <ErrorState /> : <PatientList />}
       </div>
     </>
   );
